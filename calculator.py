e72929e0cc956a37d84c710ce582cb8a1acb46c9
wraithking <pavels.grjozs@gmail.com>
2023-04-24 10:50:31 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,33 +1,34 @@
-def add(number_viens, number_divi):
+def add(number1, number2):
     """Add function."""
-    return number_viens + number_divi
+    return number1 + number2
 
-def multiplnumber_divi(number_viens, number_divi):
-    return number_viens * number_divi
+def multiplication(number1, number2):
+    """Multiplication function."""
+    return number1 * number2
 
 print("Select operation.")
-print("1.Add")
-print("2.Multiply")
+print("1. Add")
+print("2. Multiply")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2): ")
 
-    if choice in ('1', '2', '3', '4', '7'):
+    if choice in ('1', '2'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiplnumber_divi(num1, num2))
+        elif choice == '2':
+            print(num1, "*", num2, "=", multiplication(num1, num2))
 
         # check if user wants another calculation
         # break the while loop if answer is no
-        nenumber_vienst_calculation = input("Let's do nenumber_vienst calculation? (number_divies/no): ")
-        if nenumber_vienst_calculation == "no":
-          break
+        next_calculation = input("Do you want to do another calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
     
     else:
         print("Invalid Input")
