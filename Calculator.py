eed2c09bba8797cc6c94ac2c2d24884d1965bc0a
wraithking <pavels.grjozs@gmail.com>
2023-04-24 20:57:52 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,56 +1,25 @@
 # This function adds two numbers
-def add(num_1, num_2):
-    return num_1 + num_2
+def add(x, y):
+    return x + y
 
 # This function subtracts two numbers
-def subtract(num_1, num_2):
-    return num_1 - num_2
+def subtract(x, y):
+    return x - y
 
 # This function multiplies two numbers
-def multiply(num_1, num_2):
-    return num_1 * num_2
+def multiply(x, y):
+    return x * y
 
 # This function divides two numbers
-def divide(num_1, num_2):
-    return num_1 / num_2
+def divide(x, y):
+    return x / y
 
 
+# Example usage of the calculator functions without user input
+num1 = 10
+num2 = 5
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiplnum_2")
-print("4.Divide")
-
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (num_2es/no): ")
-        if next_calculation == "no":
-          break
-    else:
-        print("Invalid Input")
+print(num1, "+", num2, "=", add(num1, num2))
+print(num1, "-", num2, "=", subtract(num1, num2))
+print(num1, "*", num2, "=", multiply(num1, num2))
+print(num1, "/", num2, "=", divide(num1, num2))
