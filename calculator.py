0619f4f8e08f8be34ef64d211e2ae6388840b2c5
pavels.grjozs <pavels.grjozs@fita.lv>
2025-03-12 22:30:48 +0200

    revert to base

--- calculator.py
+++ calculator.py
@@ -1,21 +1,27 @@
 def add(number1, number2):
-    """Add function."""
     return number1 + number2
 
 def multiplication(number1, number2):
-    """Multiplication function."""
     return number1 * number2
+
+def division(number1, number2):
+    return number1 / number2
+
+def subtract(number1, number2):
+    return number1 - number2   
+
 
 print("Select operation.")
 print("1. Add")
 print("2. Multiply")
 print("3. Divide")
+print("4. Subtract")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
-    if choice in ('1', '2'):
+    if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -24,9 +30,14 @@ while True:
 
         elif choice == '2':
             print(num1, "*", num2, "=", multiplication(num1, num2))
+        
+        elif choice == '3':
+            print(num1, "/", num2, "=", division(num1, num2))  
+        
+        elif choice == '4':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+        
 
-        # check if user wants another calculation
-        # break the while loop if answer is no
         next_calculation = input("Do you want to do another calculation? (yes/no): ")
         if next_calculation == "no":
             break
